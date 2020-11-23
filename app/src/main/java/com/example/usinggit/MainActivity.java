e4f89edf9b5453add919e99662ca53636278ac29
point <z>
2020-11-23 15:20:42 +0200

    connect FireBase

--- app/src/main/java/com/example/usinggit/MainActivity.java
+++ app/src/main/java/com/example/usinggit/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String hallo = "First commit!";
+        //connect FireBase branch
     }
 }
